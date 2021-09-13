ade8dd87ae289ac997ef3a336bbb4ec3ec8515a8
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-13 19:22:08 +0200

    create action.js

---
+++ src/redux/action/Action.js
@@ -0,0 +1,25 @@
+import { SELL_LAPTOP, SELL_COMPUTER, SELL_WATCH, SELL_PHONE } from "../types/types";
+
+export const sellLaptop = () => {
+    return{}
+        type: SELL_LAPTOP
+    }
+}
+
+export const sellComputer = () => {
+    return{}
+        type: SELL_COMPUTER
+    }
+}
+
+export const sellPhone = () => {
+    return{}
+        type: SELL_PHONE
+    }
+}
+
+export const sellWatch = () => {
+    return{}
+        type: SELL_WATCH
+    }
+}
