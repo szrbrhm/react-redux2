04d4c8156e9b2bd5259e4caa8202b91f06451692
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-13 19:41:35 +0200

    create Laptop comp

---
+++ src/components/Laptop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Laptop = () => {
+    return (
+        <div>
+            <h2>Laptops Pure Redux</h2>
+            <h3>Number Of Laptops : </h3>
+        </div>
+    )
+}
+
+export default Laptop
