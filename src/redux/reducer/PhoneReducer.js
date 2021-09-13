53a97c5597fac5b0cf941f58f22928ae81d75fed
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-13 19:39:00 +0200

    create phonereducer

---
+++ src/redux/reducer/PhoneReducer.js
@@ -0,0 +1,23 @@
+import { SELL_PHONE} from "../types/Types";
+
+
+const initialState = {
+    numberOfPhone : 100,
+}
+
+const phoneReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SELL_PHONE:
+            return{
+                ...state,
+                numberOfPhone: state.numberOfPhone - 1
+            }
+        
+    
+        default:
+            return state;
+    }
+
+}
+
+export default phoneReducer;
